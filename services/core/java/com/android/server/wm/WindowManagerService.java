a60716bbaa0880c7564a6355e9bbda44985d4291
westcripp <korukaltan@gmail.com>
2015-01-24 01:13:48 +0200

    Revert "wm: further reduce default animation duration"
    This reverts commit a35ea963db022264c578c9df9184fd4755af384f.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -607,15 +607,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-<<<<<<< HEAD
     float mWindowAnimationScaleSetting = 1.0f;
     float mTransitionAnimationScaleSetting = 1.0f;
     float mAnimatorDurationScaleSetting = 1.0f;
-=======
-    float mWindowAnimationScaleSetting = 0.75f;
-    float mTransitionAnimationScaleSetting = 0.75f;
-    float mAnimatorDurationScaleSetting = 0.75f;
->>>>>>> 5fd9624... wm: further reduce default animation duration
     boolean mAnimationsDisabled = false;
 
     final InputManagerService mInputManager;
